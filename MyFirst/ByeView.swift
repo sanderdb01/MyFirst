add4b85b7660ebfe1f9760e5f29b08a746fa871c
David Sanders <sanderdb01@gmail.com>
2023-07-08 00:10:14 -0400

    first commit. Minor testing changes were made.

---
+++ MyFirst/ByeView.swift
@@ -0,0 +1,20 @@
+//
+//  ByeView.swift
+//  MyFirst
+//
+//  Created by David Sanders on 7/7/23.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+      Text("Hello again, World!")
+    }
+}
+
+struct ByeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ByeView()
+    }
+}
