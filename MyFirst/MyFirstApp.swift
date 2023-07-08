5e4d5b7a5b42e059bac5c130ea2f8ccc369b631d
David Sanders <sanderdb01@gmail.com>
2023-07-07 22:12:32 -0400

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by David Sanders on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
